88b5fe48507474a07888d5539b51a8ec2b9e98b1
Mike <kobrinets2015@mail.ru>
2023-06-14 17:32:22 +0300

    JWT authentication check and more complex error handling

--- config/settings.py
+++ config/settings.py
@@ -74,8 +74,7 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAuthenticated',
     ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        # 'rest_framework_simplejwt.authentication.JWTAuthentication',
-        # 'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
+        'src.account.backends.JWTAuthentication',
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.BasicAuthentication',
     ),
