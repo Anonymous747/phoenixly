7eb5e08424ef4d56bc1624871cde9eae423cf182
Mike <kobrinets2015@mail.ru>
2023-07-03 17:17:43 +0300

    Main entities extends

--- src/library/models.py
+++ src/library/models.py
@@ -1,17 +1,18 @@
 from django.db import models
 from django.db.models import CASCADE
 
+from src.account.models import User
+
 
 class Tag(models.Model):
     text = models.CharField(max_length=64)
 
     def __str__(self):
-        return 'Tag[id: {id}, text: {text}]'.format(
-            id=self.id, text=self.text
-        )
+        return self.text
 
 
 class Collection(models.Model):
+    user = models.ForeignKey(User, related_name='users_collection', on_delete=CASCADE, null=True)
     name = models.CharField(max_length=20)
     shareLink = models.CharField(max_length=40)
 
@@ -21,7 +22,8 @@ class Collection(models.Model):
 
 class Folder(models.Model):
     name = models.CharField(max_length=20)
-    parent_collection_id = models.ManyToManyField(Collection, related_name="folder")
+    parent_collection_id = models.ForeignKey(Collection, related_name="folders_parent_collection",
+                                             on_delete=CASCADE, null=True,)
     parent_folder = models.TextField(max_length=20)
 
     def __str__(self):
@@ -35,7 +37,7 @@ class Document(models.Model):
     date_modified = models.DateTimeField(auto_now_add=True, auto_created=True)
     language = models.CharField(max_length=4)
     tags = models.ManyToManyField(Tag)
-    related_folder = models.ManyToManyField(Folder)
+    related_folder = models.ForeignKey(Folder, related_name='document_related_folder', on_delete=CASCADE, null=True)
 
     def __str__(self):
         return self.name
@@ -45,7 +47,7 @@ class Note(models.Model):
     parent_document_id = models.TextField(max_length=20)
     title = models.TextField(max_length=30, default='')
     body = models.TextField(default='')
-    tags = models.ManyToManyField(Tag)
+    tags = models.ForeignKey(Tag, related_name='note_tags', on_delete=CASCADE, null=True)
 
     def __str__(self):
         return 'Note[id: {id}, name: {title}]'.format(id=self.id, title=self.title)
